669b0ebd8a2777eab976c1e5dfb2033eb6f33307
VanVu1104 <vanvu110404@gmail.com>
2024-09-26 22:57:31 +0700

    Done Register, Login User

---
+++ backend/controller/userController.js
@@ -0,0 +1,53 @@
+import {catchAsyncErrors} from "../middlewares/catchAsyncErrors.js"
+import ErrorHandler from "../middlewares/error.js"
+import { User } from "../models/userSchema.js";
+
+export const patientRegister = catchAsyncErrors(async(req,res,next)=>{
+    const{ firstName, lastName, email, phone, nic, dob, gender, password, role } = req.body;
+    if( !firstName ||
+        !lastName ||
+        !email ||
+        !phone ||
+        !nic ||
+        !dob ||
+        !gender || 
+        !password ||
+        !role){
+            return next(new ErrorHandler("Please Fill Full Form", 400))
+        }
+    const user = await User.findOne({ email })
+    if(user){
+        return next(new ErrorHandler("User already register", 400))
+    }
+    user = await User.create({firstName, lastName, email, phone, nic, dob, gender, password, role});
+    res.status(200).json({
+        success: true,
+        message: "user registered",
+    });
+});
+
+export const login = catchAsyncErrors(async(req,res,next) =>{
+    const { email, password, confirmpassword, role } =req.body;
+    if(!email || !password || !confirmpassword || !role){
+        return next(new ErrorHandler("Please Provide All Details!", 400))
+    }
+    if(password != confirmpassword){
+        return next(new ErrorHandler("Confirm password do not match", 400))
+    }
+    const user = await User.findOne({email}).select("+password");
+    if(!user){
+        return next(new ErrorHandler("Invalid Password or Email", 400))
+    }
+    const isPasswordMatched = await user.comparePassword(password);
+    if(!isPasswordMatched){
+        return next(new ErrorHandler("Invalid Password or Email", 400))
+    }
+    if(role != user.role){
+        return next(new ErrorHandler("User with this role not found", 400))
+
+    }
+    res.status(200).json({
+        success: true,
+        message: "User login successfully"
+    })
+})
